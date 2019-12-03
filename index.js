861e8912655db321217b5162d424bd0ef52bf5d3
Bill Leezer <bill.leezer@gmail.com>
2019-12-03 02:52:09 -0500

    created Procfile for Heroku

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
-
 const exphbs  = require('express-handlebars');
 const path =require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+
+// use body-parser middleware
+app.use(bodyParser.urlencoded({extended: false }));
+
+
+// API KEY @api = StockQuote::Stock.new(api_key: 'pk_d26d923c6b6e41648be7487d7f41b027')
+// create a call to API function
+function call_api(finishedAPI, ticker) {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_d26d923c6b6e41648be7487d7f41b027', { json: true }, (err, res, body) => { 
+ if (err) {return console.log(err);}
+  if (res.statusCode === 200){
+ 	// console.log(body); 
+ 	finishedAPI(body);
+ 		};
+ 	});
+}
 
 // Set HaandleBars Middleware
 app.engine('handlebars', exphbs());
@@ -13,12 +31,31 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "Hello there, this is the other STuff, that I was telling you about"
 
-// Set HaandleBar Routes
+// Set Main Page HaandleBar index GET Routes
 app.get('/', function (req, res) {
-    res.render('home', {stuff: otherstuff});
+	call_api(function(doneAPI) {
+		res.render('home', { stock: doneAPI 
+		});
+	}, "fb" );
 });
 
 
+// =>  call_api(function, req.body.stock_ticker)
+// Set Main Page HaandleBar index POST Routes
+app.post('/', function (req, res) {
+	call_api(function(doneAPI) {
+		 // posted_stuff = req.body.stock_ticker;
+		res.render('home', { 
+			stock: doneAPI, 
+			// posted_stuff: posted_stuff
+		 });
+	}, req.body.stock_ticker);
+});
+
+// Set About Page HaandleBar Routes
+app.get('/about.html', function (req, res) {
+    res.render('about', { stuff: otherstuff });
+});
 
 
 // Set static folder
